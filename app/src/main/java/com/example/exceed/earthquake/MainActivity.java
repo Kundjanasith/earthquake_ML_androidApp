19939fc4f81f2df9d39a86bbc787ff6679e4a512
Kundjanasith <kundjanasith.t@ku.th>
2016-07-28 21:59:14 +0700

    table color

--- app/src/main/java/com/example/exceed/earthquake/MainActivity.java
+++ app/src/main/java/com/example/exceed/earthquake/MainActivity.java
@@ -1,34 +1,71 @@
 package com.example.exceed.earthquake;
 
+import android.graphics.Color;
 import android.os.Parcelable;
+import java.util.Date;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import java.io.InputStream;
+import java.util.Calendar;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView listView;
     private ItemArrayAdapter itemArrayAdapter;
-
+    private CalendarView calendarView;
+    private Spinner dropdown;
+    private Button but;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        listView = (ListView) findViewById(R.id.list_view);
         itemArrayAdapter = new ItemArrayAdapter(getApplicationContext(), R.layout.single_list_item);
-//        Parcelable state = listView.onSaveInstanceState();
-
-//        listView.setAdapter(itemArrayAdapter);
-//        listView.onRestoreInstanceState(state);
+        calendarView = (CalendarView) findViewById(R.id.calendar);
         InputStream inputStream = getResources().openRawResource(R.raw.stats);
         CSVReader csv = new CSVReader(inputStream);
         List<String[]> scoreList = csv.read();
-        for(String[] scoreData:scoreList){
-            itemArrayAdapter.add(scoreData);
-        }
+        System.out.println("XXXX"+scoreList.get(0));
+        dropdown = (Spinner)findViewById(R.id.spinner1);
+        but = (Button)findViewById(R.id.button);
+
+
+
+//        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+//            @Override
+//            public void onSelectedDayChange(CalendarView view, int year, final int month, int dayOfMonth) {
+
+                but.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Date d = new Date(calendarView.getDateTextAppearance());
+                        Log.d("testX",d.getMonth()+"");
+                        Log.d("testY",dropdown.getSelectedItemPosition()+"");
+
+                    }
+                });
+
+//            }
+//        });
+
+
+
+
+//        String[] items = new String[]{"1", "2", "three"};
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item);
+//        dropdown.setAdapter(adapter);
+
+//        for(String[] scoreData:scoreList){
+//            itemArrayAdapter.add(scoreData);
+//        }
 
     }
 }
